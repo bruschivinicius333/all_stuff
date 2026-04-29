e811226667e95f85c1e6997932b1c7c260795cdc
bruschivinicius333 <bruschivinicius333@gmail.com>
2026-04-29 15:43:00 -0300

    Add removeDuplicates function in Solution class
    Implement a solution to remove duplicates in place from a sorted array.

---
+++ leetcode challenges/removeDuplicatesInPlace.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+    if(nums.size() <= 1) {
+        return nums.size();
+    }
+
+    for(ulong i = 0; i < nums.size() - 1; ++i) {
+        if(nums[i] == nums[i + 1]) {
+            nums.erase(nums.begin() + i--);
+        }
+    }
+
+    return nums.size();
+}
+};
